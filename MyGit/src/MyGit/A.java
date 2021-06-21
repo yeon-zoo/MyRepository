91defa1e53b001fc526d8727eceafd261eb24e8a
a1ure <a1ure@DESKTOP-47U8PSQ>
2021-06-21 23:13:03 +0900

    B branch change

--- MyGit/src/MyGit/A.java
+++ MyGit/src/MyGit/A.java
@@ -3,5 +3,6 @@ package MyGit;
 public class A {
 	public static void main(String[] agrs) {
 		System.out.println("A");
+		System.out.println("B branch change");
 	}
 }
--- MyGit/src/MyGit/B.java
+++ MyGit/src/MyGit/B.java
@@ -3,5 +3,6 @@ package MyGit;
 public class B {
 	public static void main(String[] agrs) {
 		System.out.println("B");
+		System.out.println("B branch change");
 	}
 }
